656a0a01640c943798b135a1a376f0fdcde84ca3
robertcfc <rorta@contfinco.com>
2025-11-12 02:17:09 +0000

    fix: import syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
-import pandas pd
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie!:cup_with_straw:")
