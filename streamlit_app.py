a5c6e0a754ab2d1cc0f2a173c71749472fac6b50
robertcfc <rorta@contfinco.com>
2025-11-12 00:37:26 +0000

    update session function, add gitignore

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,10 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-cnx = st.conneciton()
+cnx = st.connection("snowflake", type="snowflake")
+
+session=cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
